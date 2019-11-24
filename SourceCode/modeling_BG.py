076ef3a14a6ea5b9288d817fe1ddd5e4b716ba8b
CongSun-dlut <56787509+CongSun-dlut@users.noreply.github.com>
2019-11-24 13:33:38 +0800

    Update modeling_BG.py

--- SourceCode/modeling_BG.py
+++ SourceCode/modeling_BG.py
@@ -925,50 +925,7 @@ class BertForNextSentencePrediction(BertPreTrainedModel):
 
 
 class BertForSequenceClassification(BertPreTrainedModel):
-    """BERT model for classification.
-    This module is composed of the BERT model with a linear layer on top of
-    the pooled output.
 
-    Params:
-        `config`: a BertConfig class instance with the configuration to build a new model.
-        `num_labels`: the number of classes for the classifier. Default = 2.
-
-    Inputs:
-        `input_ids`: a torch.LongTensor of shape [batch_size, sequence_length]
-            with the word token indices in the vocabulary(see the tokens preprocessing logic in the scripts
-            `extract_features.py`, `run_classifier.py` and `run_squad.py`)
-        `token_type_ids`: an optional torch.LongTensor of shape [batch_size, sequence_length] with the token
-            types indices selected in [0, 1]. Type 0 corresponds to a `sentence A` and type 1 corresponds to
-            a `sentence B` token (see BERT paper for more details).
-        `attention_mask`: an optional torch.LongTensor of shape [batch_size, sequence_length] with indices
-            selected in [0, 1]. It's a mask to be used if the input sequence length is smaller than the max
-            input sequence length in the current batch. It's the mask that we typically use for attention when
-            a batch has varying length sentences.
-        `labels`: labels for the classification output: torch.LongTensor of shape [batch_size]
-            with indices selected in [0, ..., num_labels].
-
-    Outputs:
-        if `labels` is not `None`:
-            Outputs the CrossEntropy classification loss of the output with the labels.
-        if `labels` is `None`:
-            Outputs the classification logits of shape [batch_size, num_labels].
-
-    Example usage:
-    ```python
-    # Already been converted into WordPiece token ids
-    input_ids = torch.LongTensor([[31, 51, 99], [15, 5, 0]])
-    input_mask = torch.LongTensor([[1, 1, 1], [1, 1, 0]])
-    token_type_ids = torch.LongTensor([[0, 0, 1], [0, 1, 0]])
-
-    config = BertConfig(vocab_size_or_config_json_file=32000, hidden_size=768,
-        num_hidden_layers=12, num_attention_heads=12, intermediate_size=3072)
-
-    num_labels = 2
-
-    model = BertForSequenceClassification(config, num_labels)
-    logits = model(input_ids, token_type_ids, input_mask)
-    ```
-    """
     def __init__(self, config, num_labels):
         super(BertForSequenceClassification, self).__init__(config)
         self.num_labels = num_labels
@@ -977,15 +934,10 @@ class BertForSequenceClassification(BertPreTrainedModel):
         self.classifier = nn.Linear(3*config.hidden_size, num_labels)
         self.apply(self.init_bert_weights)
 
-        # self.gate = nn.Linear(config.hidden_size, config.hidden_size)
-        # self.sigmoid = nn.Sigmoid()
-
 
     def forward(self, input_ids=None, token_type_ids=None, attention_mask=None, P_gauss1_bs=None, P_gauss2_bs=None, labels=None):
 
         input_token_output, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
-
-        # logits = self.classifier(pooled_output)
         
         P_gauss1_bs=P_gauss1_bs.unsqueeze(dim=1)
         P_gauss2_bs=P_gauss2_bs.unsqueeze(dim=1)
@@ -995,7 +947,6 @@ class BertForSequenceClassification(BertPreTrainedModel):
 
         gauss_entity1=gauss_entity1.squeeze(dim=1)
         gauss_entity2=gauss_entity2.squeeze(dim=1)
-        # print('gauss_entity1.size()',gauss_entity1.size())
 
         gauss_entity1 = self.dropout(gauss_entity1)
         gauss_entity2 = self.dropout(gauss_entity2)
